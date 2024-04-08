37f11f51ccd4d18af46b8b00d03db318a1ebff1b
sweeetpotaooo <sehyun5004@naver.com>
2024-04-09 01:38:31 +0900

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { headerNav } from "../constants";
 
 const Header = () => {
+    const [show, setShow] = useState(false);
+
+    const toggleMenu = () => {
+        setShow((prevShow) => !prevShow);
+    }
+
     return (
         <header id="header" role="banner">
             <div className="header__inner">
                 <div className="header__logo">
-                    <a href="/">portfolio<em>vite</em></a>
+                    <h1>
+                        <a href="/">KUMMATE<em>완벽한 룸메이트</em></a>
+                    </h1>
                 </div>
-                <nav className="header__nav" role="navigation" aria-label="메인 메뉴">
+                <nav 
+                    className={`header__nav ${show ? "show" : ""}`} 
+                    role="navigation" 
+                    aria-label="메인메뉴"
+                >
                     <ul>
-                        <li><a href="#intro">intro</a></li>
-                        <li><a href="#skill">skill</a></li>
-                        <li><a href="#site">site</a></li>
-                        <li><a href="#port">portfolio</a></li>
-                        <li><a href="#contact">contact</a></li>
+                        {headerNav.map((nav, key) => (
+                            <li key={key}>
+                                <a href={nav.url}>{nav.title}</a>
+                            </li>
+                        ))}
                     </ul>
                 </nav>
                 <div 
                     className="header__nav__mobile" 
                     id="headerToggle" 
                     aria-controls="primary-menu" 
-                    aria-expanded="false" 
-                    role="button" 
-                    tabindex="0"
+                    aria-expanded={show ? "true" : "false"} 
+                    role="button"
+                    tabIndex="0"
+                    onClick={toggleMenu}
                 >
                     <span></span>
                 </div>
             </div>
         </header>
-    );
-};
+    )
+}
 
 export default Header;
---
+++ src/constants/index.js
@@ -0,0 +1,52 @@
+export const headerNav = [
+  {
+      title: "룸메이트 찾기",
+      url: "#intro"
+  },
+  {
+      title: "마이페이지",
+      url: "#skill"
+  },
+  {
+    title: "로그인",
+    url: "#skill"
+  } 
+];
+
+export const footerText = [
+  {
+      title: "youtube",
+      desc: "유튜브에 오시면 더 많은 강의를 볼 수 있습니다.",
+      link: "https://www.youtube.com/@Webstoryboy",
+  },
+  {
+      title: "github",
+      desc: "깃헙에 오시면 더 많은 소스를 볼 수 있습니다.",
+      link: "https://github.com/webstoryboy",
+  },
+  {
+      title: "blog",
+      desc: "블러그에 오시면 더 많은 정보를 볼 수 있습니다.",
+      link: "https://webstoryboy.co.kr",
+  },
+  {
+      title: "gsap",
+      desc: "GSAP에 오시면 더 많은 강의를 볼 수 있습니다.",
+      link: "https://www.youtube.com/playlist?list=PL4UVBBIc6giL8-6jvrClimg0cFL-Muqiq",
+  },
+  {
+      title: "react",
+      desc: "리액트로 만든 사이트를 같이 만들어 봅니다.",
+      link: "https://github.com/webstoryboy/port2023-react",
+  },
+  {
+      title: "vue",
+      desc: "뷰로 만든 사이트를 같이 만들어 봅니다.",
+      link: "https://github.com/webstoryboy/port2023-vue",
+  },
+  {
+      title: "next",
+      desc: "넥스트로 만든 사이트를 같이 만들어 봅니다.",
+      link: "https://github.com/webstoryboy/port2023-next",
+  },
+];
