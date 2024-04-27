c4f1630f319a32e4109720c0fec67abd37562ea3
sweeetpotaooo <sehyun5004@naver.com>
2024-04-27 23:24:24 +0900

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,18 +7,14 @@ const Footer = () => {
         <footer id="footer" role="contentinfo">
             <div className="footer__inner">
                 <h2 className="footer__text">
-                    <div>webstoryboy</div>
-                    <div>@webs</div>
                 </h2>
                 <div className="footer__info">
                     <div className="left">
                         <div className="title">
-                            <a href="/">sign up</a>
                         </div>
                         <p className="desc"></p>
                     </div>
                     <div className="right">
-                        <h3>social</h3>
                     </div>
                 </div>
                 <div className="footer__right">
